d48a310954cc985cb6354af62661955758937585
Edwin Marshall <emarshall85@gmail.com>
2018-08-15 04:43:07 -0400

    variable name substitution

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 def fib(i):
-    """ Return the nth fibonacci number, starting with 1. """
+    """ Return the ith fibonacci number, starting with 1. """
     if i < 1:
         raise Exception("Cannot commute fibonacci when n < 1")
     elif i <= 2:
@@ -11,11 +11,10 @@ def fib(i):
         return fib(i - 1) + fib(i - 2)
 
 
-def fibs(i):
-    """ Return a list of the first n fibonacci numbers. """
-    import ipdb; ipdb.set_trace()
-    return [fib(x) for x in range(1, i + 1)]
+def fibs(x):
+    """ Return a list of the first x fibonacci numbers. """
+    return [fib(y) for y in range(1, x + 1)]
 
 
 if __name__ == "__main__":
-    print fibs(20)
+    print fibs(8)
